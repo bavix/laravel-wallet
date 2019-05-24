a0eb0ac7d82cc4a30c715325f1da1131a536853b
Babichev Maxim <info@babichev.net>
2019-05-25 00:18:55 +0300

    update customer

--- src/Interfaces/Customer.php
+++ src/Interfaces/Customer.php
@@ -3,6 +3,7 @@
 namespace Bavix\Wallet\Interfaces;
 
 use Bavix\Wallet\Models\Transfer;
+use Bavix\Wallet\Objects\Cart;
 
 interface Customer extends Wallet
 {
@@ -60,4 +61,51 @@ interface Customer extends Wallet
      * @return bool
      */
     public function forceRefund(Product $product, bool $gifts = null): bool;
+
+    /**
+     * @param Cart $cart
+     * @param bool $force
+     * @return Transfer[]
+     * @throws
+     */
+    public function payCart(Cart $cart, bool $force = null): array;
+
+    /**
+     * @param Cart $cart
+     * @param bool $force
+     * @return Transfer[]
+     * @throws
+     */
+    public function safePayCart(Cart $cart, bool $force = null): array;
+
+    /**
+     * @param Cart $cart
+     * @return Transfer[]
+     * @throws
+     */
+    public function forcePayCart(Cart $cart): array;
+
+    /**
+     * @param Cart $cart
+     * @param bool $force
+     * @param bool $gifts
+     * @return bool
+     * @throws
+     */
+    public function refundCart(Cart $cart, bool $force = null, bool $gifts = null): bool;
+
+    /**
+     * @param Cart $cart
+     * @param bool $force
+     * @param bool $gifts
+     * @return bool
+     */
+    public function safeRefundCart(Cart $cart, bool $force = null, bool $gifts = null): bool;
+
+    /**
+     * @param Cart $cart
+     * @param bool $gifts
+     * @return bool
+     */
+    public function forceRefundCart(Cart $cart, bool $gifts = null): bool;
 }
