e61426931133671f10197add57e708213212a42c
Babichev Maxim <info@babichev.net>
2019-06-23 12:53:39 +0300

    add canConfirm

---
+++ src/Exceptions/WalletOwnerInvalid.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bavix\Wallet\Exceptions;
+
+use InvalidArgumentException;
+
+class WalletOwnerInvalid extends InvalidArgumentException
+{
+
+}
---
+++ src/Traits/CanConfirm.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace Bavix\Wallet\Traits;
+
+use Bavix\Wallet\Exceptions\ConfirmedInvalid;
+use Bavix\Wallet\Exceptions\WalletOwnerInvalid;
+use Bavix\Wallet\Models\Transaction;
+use Bavix\Wallet\Services\CommonService;
+use Bavix\Wallet\Services\WalletService;
+use Illuminate\Support\Facades\DB;
+
+trait CanConfirm
+{
+
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     */
+    public function confirm(Transaction $transaction): bool
+    {
+        return DB::transaction(function () use ($transaction) {
+            $wallet = app(WalletService::class)
+                ->getWallet($this);
+
+            if (!$wallet->refreshBalance()) {
+                return false;
+            }
+
+            if ($transaction->type === Transaction::TYPE_WITHDRAW) {
+                app(CommonService::class)->verifyWithdraw(
+                    $wallet,
+                    \abs($transaction->amount)
+                );
+            }
+
+            // update balance
+            return app(CommonService::class)
+                ->addBalance($wallet, $transaction->amount) &&
+                
+                // confirm
+                $this->forceConfirm($transaction);
+        });
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     */
+    public function safeConfirm(Transaction $transaction): bool
+    {
+        try {
+            return $this->confirm($transaction);
+        } catch (\Throwable $throwable) {
+            return false;
+        }
+    }
+
+    /**
+     * @param Transaction $transaction
+     * @return bool
+     * @throws ConfirmedInvalid
+     * @throws WalletOwnerInvalid
+     */
+    public function forceConfirm(Transaction $transaction): bool
+    {
+        $wallet = app(WalletService::class)
+            ->getWallet($this);
+
+        if ($transaction->confirmed) {
+            throw new ConfirmedInvalid(); // todo
+        }
+
+        if ($wallet->id !== $transaction->wallet_id) {
+            throw new WalletOwnerInvalid(); // todo
+        }
+
+        return $transaction->update(['confirmed' => true]);
+    }
+
+}
