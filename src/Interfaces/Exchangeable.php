becb737bb84c7dfd37da387ac5f8be40e0b0737a
Babichev Maxim <babichev@mail.tvil.ru>
2019-06-25 15:51:34 +0300

    add meta

--- src/Interfaces/Exchangeable.php
+++ src/Interfaces/Exchangeable.php
@@ -9,21 +9,24 @@ interface Exchangeable
     /**
      * @param Wallet $to
      * @param int $amount
+     * @param array|null $meta
      * @return Transfer
      */
-    public function exchange(Wallet $to, int $amount): Transfer;
+    public function exchange(Wallet $to, int $amount, ?array $meta = null): Transfer;
 
     /**
      * @param Wallet $to
      * @param int $amount
+     * @param array|null $meta
      * @return Transfer|null
      */
-    public function safeExchange(Wallet $to, int $amount): ?Transfer;
+    public function safeExchange(Wallet $to, int $amount, ?array $meta = null): ?Transfer;
 
     /**
      * @param Wallet $to
      * @param int $amount
+     * @param array|null $meta
      * @return Transfer
      */
-    public function forceExchange(Wallet $to, int $amount): Transfer;
+    public function forceExchange(Wallet $to, int $amount, ?array $meta = null): Transfer;
 }
--- src/Traits/CanExchange.php
+++ src/Traits/CanExchange.php
@@ -16,7 +16,7 @@ trait CanExchange
     /**
      * @inheritDoc
      */
-    public function exchange(Wallet $to, int $amount): Transfer
+    public function exchange(Wallet $to, int $amount, ?array $meta = null): Transfer
     {
         $wallet = app(WalletService::class)
             ->getWallet($this);
@@ -24,16 +24,16 @@ trait CanExchange
         app(CommonService::class)
             ->verifyWithdraw($wallet, $amount);
 
-        return $this->forceExchange($to, $amount);
+        return $this->forceExchange($to, $amount, $meta);
     }
 
     /**
      * @inheritDoc
      */
-    public function safeExchange(Wallet $to, int $amount): ?Transfer
+    public function safeExchange(Wallet $to, int $amount, ?array $meta = null): ?Transfer
     {
         try {
-            return $this->exchange($to, $amount);
+            return $this->exchange($to, $amount, $meta);
         } catch (\Throwable $throwable) {
             return null;
         }
@@ -42,18 +42,18 @@ trait CanExchange
     /**
      * @inheritDoc
      */
-    public function forceExchange(Wallet $to, int $amount): Transfer
+    public function forceExchange(Wallet $to, int $amount, ?array $meta = null): Transfer
     {
         /**
          * @var Wallet $from
          */
         $from = app(WalletService::class)->getWallet($this);
 
-        return DB::transaction(function() use ($from, $to, $amount) {
+        return DB::transaction(function() use ($from, $to, $amount, $meta) {
             $rate = app(ExchangeService::class)->rate($from, $to);
             $fee = app(WalletService::class)->fee($to, $amount);
-            $withdraw = $from->forceWithdraw($amount + $fee);
-            $deposit = $to->deposit($amount * $rate);
+            $withdraw = $from->forceWithdraw($amount + $fee, $meta);
+            $deposit = $to->deposit($amount * $rate, $meta);
 
             $transfers = app(CommonService::class)->multiBrings([
                 (new Bring())
