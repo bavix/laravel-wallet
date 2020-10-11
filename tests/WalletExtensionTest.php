f083aa67fa265c6e39ec0dbffa7d3a5c48fe41f4
Babichev Maxim <maksim.babichev95@gmail.com>
2020-10-11 12:11:05 +0300

    add test for package cknow/laravel-money

--- tests/WalletExtensionTest.php
+++ tests/WalletExtensionTest.php
@@ -4,6 +4,7 @@ namespace Bavix\Wallet\Test;
 
 use Bavix\Wallet\Objects\Operation;
 use Bavix\Wallet\Test\Common\Models\Transaction;
+use Bavix\Wallet\Test\Common\Models\TransactionMoney;
 use Bavix\Wallet\Test\Factories\BuyerFactory;
 use Bavix\Wallet\Test\Models\Buyer;
 
@@ -35,6 +36,30 @@ class WalletExtensionTest extends TestCase
         self::assertEquals('VietComBank', $transaction->bank_method);
     }
 
+    /**
+     * @return void
+     */
+    public function testTransactionMoneyAttribute(): void
+    {
+        $this->app['config']->set('wallet.transaction.model', TransactionMoney::class);
+
+        /**
+         * @var Buyer $buyer
+         * @var TransactionMoney $transaction
+         */
+        $buyer = BuyerFactory::new()->create();
+        self::assertFalse($buyer->relationLoaded('wallet'));
+        $transaction = $buyer->deposit(1000, ['currency' => 'EUR']);
+
+        self::assertEquals($transaction->amount, $buyer->balance);
+        self::assertInstanceOf(TransactionMoney::class, $transaction);
+        self::assertEquals(1000, $transaction->currency->getAmount());
+        self::assertEquals('EUR', $transaction->currency->getCurrency()->getCode());
+    }
+
+    /**
+     * @return void
+     */
     public function testNoCustomAttribute(): void
     {
         /**
