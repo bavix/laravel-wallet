b6d8a3ad90958b17f38d5ba0dab0d11d72533bcd
Babichev Maxim <rez1dent3@users.noreply.github.com>
2020-10-30 17:12:18 +0000

    Apply fixes from StyleCI
    [ci skip] [skip ci]

--- database/migrations_v2/2020_10_30_193412_add_meta_wallets_table.php
+++ database/migrations_v2/2020_10_30_193412_add_meta_wallets_table.php
@@ -2,11 +2,11 @@
 
 use Bavix\Wallet\Models\Wallet;
 use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\MySqlConnection;
 use Illuminate\Database\PostgresConnection;
+use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Fluent as ColumnDefinition;
 
 class AddMetaWalletsTable extends Migration
