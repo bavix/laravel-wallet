8a02ddef04458601c25885bff52c83285414c37e
Babichev Maxim <maksim.babichev95@gmail.com>
2020-11-18 18:22:18 +0300

    fix bug

--- src/Services/CommonService.php
+++ src/Services/CommonService.php
@@ -239,7 +239,9 @@ class CommonService
                 ->incBalance($wallet, $amount);
 
             try {
-                $result = $wallet->update(compact('balance'));
+                $result = $wallet->newQuery()
+                    ->whereKey($wallet->getKey())
+                    ->update(compact('balance'));
             } catch (\Throwable $throwable) {
                 app(Storable::class)
                     ->setBalance($wallet, $wallet->getAvailableBalance());
@@ -247,7 +249,10 @@ class CommonService
                 throw $throwable;
             }
 
-            if (! $result) {
+            if ($result) {
+                $wallet->fill(compact('balance'))
+                    ->syncOriginalAttributes('balance');
+            } else {
                 app(Storable::class)
                     ->setBalance($wallet, $wallet->getAvailableBalance());
             }
