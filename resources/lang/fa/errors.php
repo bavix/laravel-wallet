7533f05ba02a28f59490866ef8cb0462450b1885
Hadi Sharghi <hsharghi@icloud.com>
2021-04-13 17:16:00 +0430

    add Farsi translations

---
+++ resources/lang/fa/errors.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'price_positive' => 'قیمت باید عددی مثبت باشد',
+    'product_stock' => 'کالا موجود نیست',
+    'wallet_empty' => 'کیف پول خالی است',
+    'insufficient_funds' => 'موجودی ناکافی',
+    'confirmed_invalid' => 'تراکنش قبلا تایید شده است',
+    'unconfirmed_invalid' => 'تاییدیه تراکنش لغو شده است',
+    'owner_invalid' => 'این کیف پول متعلق به شما نیست',
+];
