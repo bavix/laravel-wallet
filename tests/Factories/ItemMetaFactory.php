b143ee619c0d7e1d87218845711cc25ab48a40be
Babichev Maxim <maksim.babichev95@gmail.com>
2021-04-17 23:10:54 +0300

    #318 Added transfer of meta-information through the cart

---
+++ tests/Factories/ItemMetaFactory.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Bavix\Wallet\Test\Factories;
+
+use Bavix\Wallet\Test\Models\ItemMeta;
+
+class ItemMetaFactory extends ItemFactory
+{
+    protected $model = ItemMeta::class;
+}
