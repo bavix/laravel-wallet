0af399fe70ee546fd78c258f4c3a61d3b54826ff
Babichev Maxim <maksim.babichev95@gmail.com>
2021-04-24 00:27:52 +0300

    update FeeService

--- src/Services/FeeService.php
+++ src/Services/FeeService.php
@@ -4,17 +4,30 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Services;
 
+use Bavix\Wallet\Contracts\MathInterface;
 use Bavix\Wallet\Interfaces\MinimalTaxable;
 use Bavix\Wallet\Interfaces\Taxable;
 use Bavix\Wallet\Interfaces\Wallet;
 
 class FeeService
 {
+    private FloatService $floatService;
+
+    private MathInterface $mathService;
+
+    public function __construct(
+        FloatService $floatService,
+        MathInterface $mathService
+    ) {
+        $this->floatService = $floatService;
+        $this->mathService = $mathService;
+    }
+
     public function fee(Wallet $wallet, $amount)
     {
         $fee = 0;
         if ($wallet instanceof Taxable) {
-            $placesValue = $this->decimalPlacesValue($wallet);
+            $placesValue = $this->floatService->exponent($wallet);
             $fee = $this->mathService->floor(
                 $this->mathService->div(
                     $this->mathService->mul($amount, $wallet->getFeePercent(), 0),
