8f0d3bcf81b53bd03fe450a571b26c34c7a95d56
Maxim Babichev <maksim.babichev95@gmail.com>
2021-09-28 21:36:31 +0300

    merge migrations

---
+++ database/2018_11_07_192923_create_transfers_table.php
@@ -0,0 +1,77 @@
+<?php
+
+declare(strict_types=1);
+
+use Bavix\Wallet\Models\Transaction;
+use Bavix\Wallet\Models\Transfer;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransfersTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create($this->table(), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->morphs('from');
+            $table->morphs('to');
+            $table
+                ->enum(
+                    'status',
+                    ['exchange', 'transfer', 'paid', 'refund', 'gift']
+                )
+                ->default('transfer')
+            ;
+
+            $table
+                ->enum(
+                    'status_last',
+                    ['exchange', 'transfer', 'paid', 'refund', 'gift']
+                )
+                ->nullable()
+            ;
+
+            $table->unsignedBigInteger('deposit_id');
+            $table->unsignedBigInteger('withdraw_id');
+
+            $table->decimal('discount', 64, 0)
+                ->default(0)
+            ;
+
+            $table->decimal('fee', 64, 0)
+                ->default(0)
+            ;
+
+            $table->uuid('uuid')->unique();
+            $table->timestamps();
+
+            $table->foreign('deposit_id')
+                ->references('id')
+                ->on($this->transactionTable())
+                ->onDelete('cascade')
+            ;
+
+            $table->foreign('withdraw_id')
+                ->references('id')
+                ->on($this->transactionTable())
+                ->onDelete('cascade')
+            ;
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::drop($this->table());
+    }
+
+    private function table(): string
+    {
+        return (new Transfer())->getTable();
+    }
+
+    private function transactionTable(): string
+    {
+        return (new Transaction())->getTable();
+    }
+}
--- ecs.php
+++ ecs.php
@@ -21,6 +21,7 @@ return static function (ContainerConfigurator $containerConfigurator): void {
 
     $parameters = $containerConfigurator->parameters();
     $parameters->set(Option::PATHS, [
+        __DIR__ . '/database',
         __DIR__ . '/src',
         __DIR__ . '/tests',
     ]);
