66f5fb634e763615d3777f20781ec03d3fd355c1
Maxim Babichev <maksim.babichev95@gmail.com>
2021-11-04 00:18:27 +0300

    non-empty-array

--- src/Services/CommonService.php
+++ src/Services/CommonService.php
@@ -10,8 +10,10 @@ use Bavix\Wallet\Interfaces\Wallet;
 use Bavix\Wallet\Internal\Assembler\TransferDtoAssembler;
 use Bavix\Wallet\Internal\BookkeeperInterface;
 use Bavix\Wallet\Internal\ConsistencyInterface;
+use Bavix\Wallet\Internal\Dto\TransferDto;
 use Bavix\Wallet\Internal\MathInterface;
 use Bavix\Wallet\Internal\Repository\TransferRepository;
+use Bavix\Wallet\Internal\Service\CastService;
 use Bavix\Wallet\Models\Transaction;
 use Bavix\Wallet\Models\Transfer;
 use Bavix\Wallet\Models\Wallet as WalletModel;
@@ -26,24 +28,30 @@ class CommonService
     private DbService $dbService;
     private LockService $lockService;
     private MathInterface $math;
+    private CastService $castService;
     private WalletService $walletService;
     private BookkeeperInterface $bookkeeper;
     private ConsistencyInterface $consistency;
+    private TransferDtoAssembler $transferDtoAssembler;
 
     public function __construct(
         DbService $dbService,
         LockService $lockService,
         MathInterface $math,
+        CastService $castService,
         WalletService $walletService,
         BookkeeperInterface $bookkeeper,
-        ConsistencyInterface $consistency
+        ConsistencyInterface $consistency,
+        TransferDtoAssembler $transferDtoAssembler
     ) {
         $this->dbService = $dbService;
         $this->lockService = $lockService;
         $this->math = $math;
+        $this->castService = $castService;
         $this->walletService = $walletService;
         $this->bookkeeper = $bookkeeper;
         $this->consistency = $consistency;
+        $this->transferDtoAssembler = $transferDtoAssembler;
     }
 
     /**
@@ -203,25 +211,25 @@ class CommonService
     /**
      * Create Bring without DB::transaction.
      *
-     * @param Bring[] $brings
+     * @param non-empty-array<mixed, Bring> $brings
      *
      * @deprecated
      */
     public function multiBrings(array $brings): array
     {
         return $this->lockService->lock($this, __FUNCTION__, function () use ($brings) {
-            $objects = [];
-            foreach ($brings as $bring) {
-                $objects[$bring->getUuid()] = app(TransferDtoAssembler::class)->create(
+            $objects = array_map(
+                fn (Bring $bring): TransferDto => $this->transferDtoAssembler->create(
                     $bring->getDeposit()->getKey(),
                     $bring->getWithdraw()->getKey(),
                     $bring->getStatus(),
-                    $bring->getFrom(),
-                    $bring->getTo(),
+                    $this->castService->getModel($bring->getFrom()),
+                    $this->castService->getModel($bring->getTo()),
                     $bring->getDiscount(),
                     $bring->getFee()
-                );
-            }
+                ),
+                $brings
+            );
 
             return app(TransferRepository::class)->insert($objects);
         });
