650b9289d6283794a6bf603995328a362dee9a2c
Maxim Babichev <maksim.babichev95@gmail.com>
2021-11-09 20:08:15 +0300

    update .phpstorm.meta.php

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -2,26 +2,45 @@
 
 namespace PHPSTORM_META {
 
-    use Bavix\Wallet\Interfaces\Mathable;
-    use Bavix\Wallet\Interfaces\Rateable;
-    use Bavix\Wallet\Interfaces\Storable;
-    use Bavix\Wallet\Models\Transaction;
-    use Bavix\Wallet\Models\Transfer;
-    use Bavix\Wallet\Models\Wallet;
+    use app;
+    use Bavix\Wallet\Internal\BasketInterface;
+    use Bavix\Wallet\Internal\BookkeeperInterface;
+    use Bavix\Wallet\Internal\CartInterface;
+    use Bavix\Wallet\Internal\ConsistencyInterface;
+    use Bavix\Wallet\Internal\ExchangeInterface;
+    use Bavix\Wallet\Internal\LockInterface;
+    use Bavix\Wallet\Internal\MathInterface;
+    use Bavix\Wallet\Internal\PurchaseInterface;
+    use Bavix\Wallet\Internal\StorageInterface;
+    use Bavix\Wallet\Internal\UuidInterface;
     use Bavix\Wallet\Objects\Cart;
-    use Bavix\Wallet\Objects\EmptyLock;
-    use Bavix\Wallet\Objects\Operation;
     use Bavix\Wallet\Services\CommonService;
+    use Bavix\Wallet\Services\DbService;
+    use Bavix\Wallet\Services\LockService;
+    use Bavix\Wallet\Services\MetaService;
     use Bavix\Wallet\Services\WalletService;
 
-    override(\app(0), map([
+    override(app(0), map([
+        BasketInterface::class => BasketInterface::class,
+        BookkeeperInterface::class => BookkeeperInterface::class,
+        CartInterface::class => CartInterface::class,
+        ConsistencyInterface::class => ConsistencyInterface::class,
+        ExchangeInterface::class => ExchangeInterface::class,
+        LockInterface::class => LockInterface::class,
+        MathInterface::class => MathInterface::class,
+        PurchaseInterface::class => PurchaseInterface::class,
+        StorageInterface::class => StorageInterface::class,
+        UuidInterface::class => UuidInterface::class,
+
+        // deprecated's
         Cart::class => Cart::class,
-        ExchangeService::class => ExchangeService::class,
+
+        // old
         CommonService::class => CommonService::class,
+        DbService::class => DbService::class,
+        LockService::class => LockService::class,
+        MetaService::class => MetaService::class,
         WalletService::class => WalletService::class,
-        Wallet::class => Wallet::class,
-        Transfer::class => Transfer::class,
-        Transaction::class => Transaction::class,
     ]));
 
 }
