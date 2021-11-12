0ecab599cf3d73c2ec0e6971cd52fe1f76af20ee
Maxim Babichev <maksim.babichev95@gmail.com>
2021-11-12 19:06:10 +0300

    declare strict in units

--- tests/Objects/TransactionDtoTransformer.php
+++ tests/Objects/TransactionDtoTransformer.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Bavix\Wallet\Test\Objects;
 
 use Bavix\Wallet\Internal\Dto\TransactionDto;
--- tests/migrations/2019_09_19_191432_alter_column_transaction_table.php
+++ tests/migrations/2019_09_19_191432_alter_column_transaction_table.php
@@ -1,5 +1,7 @@
 <?php
 
+declare(strict_types=1);
+
 use Bavix\Wallet\Test\Common\Models\Transaction;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
