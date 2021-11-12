0a1434e83e9984a6e4858ba7573b9fce719ba54f
Maxim Babichev <maksim.babichev95@gmail.com>
2021-11-12 22:39:05 +0300

    update config.php

--- config/config.php
+++ config/config.php
@@ -8,30 +8,37 @@ use Bavix\Wallet\Models\Wallet;
 use Bavix\Wallet\Services\AtomicService;
 use Bavix\Wallet\Services\BasketService;
 use Bavix\Wallet\Services\BookkeeperService;
-use Bavix\Wallet\Services\CommonService;
 use Bavix\Wallet\Services\ConsistencyService;
-use Bavix\Wallet\Services\DbService;
 use Bavix\Wallet\Services\ExchangeService;
-use Bavix\Wallet\Services\LockService;
 use Bavix\Wallet\Services\MathService;
-use Bavix\Wallet\Services\MetaService;
 use Bavix\Wallet\Services\PurchaseService;
 use Bavix\Wallet\Services\StorageService;
 use Bavix\Wallet\Services\UuidFactoryService;
-use Bavix\Wallet\Services\WalletService;
 
 return [
-    // infra settings
-    'cache' => ['driver' => null],
+    /**
+     * Arbitrary Precision Calculator.
+     *
+     *  'scale' - length of the mantissa
+     */
+    'math' => ['scale' => 64],
+
+    /**
+     * Storage of the state of the balance of wallets.
+     */
+    'cache' => ['driver' => 'array'],
+
+    /**
+     * A system for dealing with race conditions.
+     */
     'lock' => [
         'driver' => null,
         'seconds' => 1,
     ],
 
-    // long arithmetic
-    'math' => ['scale' => 64],
-
-    // service overload
+    /**
+     * Services that can be overloaded.
+     */
     'services' => [
         'basket' => BasketService::class,
         'bookkeeper' => BookkeeperService::class,
@@ -44,25 +51,25 @@ return [
         'uuid' => UuidFactoryService::class,
     ],
 
-    // legacy-service overload
-    'legacy' => [
-        'common' => CommonService::class,
-        'wallet' => WalletService::class,
-        'db' => DbService::class,
-        'lock' => LockService::class,
-        'meta' => MetaService::class,
-    ],
-
+    /**
+     * Base model 'transaction'.
+     */
     'transaction' => [
         'table' => 'transactions',
         'model' => Transaction::class,
     ],
 
+    /**
+     * Base model 'transfer'.
+     */
     'transfer' => [
         'table' => 'transfers',
         'model' => Transfer::class,
     ],
 
+    /**
+     * Base model 'wallet'.
+     */
     'wallet' => [
         'table' => 'wallets',
         'model' => Wallet::class,
