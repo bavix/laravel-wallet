ac493e993780450688cebe3d97466468cc92130d
Maxim Babichev <maksim.babichev95@gmail.com>
2021-11-28 19:54:36 +0300

    micro-optimize

--- src/Services/StateService.php
+++ src/Services/StateService.php
@@ -4,22 +4,26 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Services;
 
+use Bavix\Wallet\Internal\Service\MathServiceInterface;
 use Bavix\Wallet\Models\Wallet;
 
 final class StateService implements StateServiceInterface
 {
     private BookkeeperServiceInterface $bookkeeperService;
     private RegulatorServiceInterface $regulatorService;
+    private MathServiceInterface $mathService;
 
     /** @var Wallet[] */
     private array $wallets = [];
 
     public function __construct(
         BookkeeperServiceInterface $bookkeeperService,
-        RegulatorServiceInterface $regulatorService
+        RegulatorServiceInterface $regulatorService,
+        MathServiceInterface $mathService
     ) {
         $this->bookkeeperService = $bookkeeperService;
         $this->regulatorService = $regulatorService;
+        $this->mathService = $mathService;
     }
 
     public function persist(Wallet $wallet): void
@@ -31,10 +35,17 @@ final class StateService implements StateServiceInterface
     {
         $flags = [];
         foreach ($this->wallets as $wallet) {
-            if ($flags[$wallet->uuid] ?? true) {
-                $this->bookkeeperService->increase($wallet, $this->regulatorService->diff($wallet));
-                $flags[$wallet->uuid] = false;
+            if ($flags[$wallet->uuid] ?? false) {
+                continue;
             }
+
+            $diffValue = $this->regulatorService->diff($wallet);
+            if ($this->mathService->compare($diffValue, 0) === 0) {
+                continue;
+            }
+
+            $this->bookkeeperService->increase($wallet, $diffValue);
+            $flags[$wallet->uuid] = true;
         }
 
         $this->purge();
