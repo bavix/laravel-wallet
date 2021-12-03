637e6457b11d4fed741eaca13eeaf8ef7e78338c
Maxim Babichev <maksim.babichev95@gmail.com>
2021-12-03 19:52:22 +0300

    add WalletCreatedEvent

---
+++ src/Services/WalletServiceInterface.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bavix\Wallet\Services;
+
+use Bavix\Wallet\Internal\Exceptions\ModelNotFoundException;
+use Bavix\Wallet\Models\Wallet;
+use Illuminate\Database\Eloquent\Model;
+
+interface WalletServiceInterface
+{
+    public function create(Model $model, array $data): Wallet;
+
+    public function findBySlug(Model $model, string $slug): ?Wallet;
+
+    public function findByUuid(string $uuid): ?Wallet;
+
+    public function findById(int $id): ?Wallet;
+
+    /** @throws ModelNotFoundException */
+    public function getBySlug(Model $model, string $slug): Wallet;
+
+    /** @throws ModelNotFoundException */
+    public function getByUuid(string $uuid): Wallet;
+
+    /** @throws ModelNotFoundException */
+    public function getById(int $id): Wallet;
+}
