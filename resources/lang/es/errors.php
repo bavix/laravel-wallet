83651a63f0d3a674f0c3a30266bfee85a508b6b2
Eduardo Montaño <emontano@bit.lat>
2022-02-21 12:54:02 -0600

    Spanish Error Transalate

---
+++ resources/lang/es/errors.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'price_positive' => 'El precio debe ser un valor positivo',
+    'product_stock' => 'El producto no esta disponible en el inventario',
+    'wallet_empty' => 'La cartera esta vacía',
+    'insufficient_funds' => 'Fondos Insuficientes',
+    'confirmed_invalid' => 'La transferencia ya fue confirmada',
+    'unconfirmed_invalid' => 'La confirmación ya fue restablecida',
+    'owner_invalid' => 'No eres el propietario de esta cartera',
+];
