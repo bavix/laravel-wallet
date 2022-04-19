34909d476ac938d544f90fbdaf46ddb1830a4d81
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-19 20:45:01 +0300

    add confirmed in exchange/transfer

--- src/Internal/Assembler/ExtraDtoAssemblerInterface.php
+++ src/Internal/Assembler/ExtraDtoAssemblerInterface.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Internal\Assembler;
 
-use Bavix\Wallet\External\ExtraDtoInterface;
+use Bavix\Wallet\External\Contracts\ExtraDtoInterface;
 
 interface ExtraDtoAssemblerInterface
 {
