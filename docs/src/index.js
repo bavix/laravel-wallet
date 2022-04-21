08122fdbfb8edf73643cedc86ae57fabd0a8529b
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-21 21:16:02 +0300

    gtag

--- docs/src/index.js
+++ docs/src/index.js
@@ -1,6 +1,7 @@
 import 'docsify'
-import 'docsify/lib/plugins/ga'
+// import 'docsify/lib/plugins/ga'
 import './cozy'
+import './gtag'
 import 'docsify/lib/plugins/emoji'
 import 'docsify/lib/plugins/search'
 import 'prismjs/components/prism-php'
