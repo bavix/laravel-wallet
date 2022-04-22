a54ac86d343dc7aaff98f7ab2778168798c765f8
Maxim Babichev <maksim.babichev95@gmail.com>
2022-04-22 22:12:39 +0300

    An old bug that prevented full uuid support.

--- tests/Units/Domain/DiscountTest.php
+++ tests/Units/Domain/DiscountTest.php
@@ -60,12 +60,13 @@ final class DiscountTest extends TestCase
 
         self::assertInstanceOf(Buyer::class, $transfer->from->holder);
         self::assertInstanceOf(Wallet::class, $transfer->from);
-        self::assertInstanceOf(Item::class, $transfer->to);
+        self::assertInstanceOf(Item::class, $transfer->to->holder);
         self::assertInstanceOf(Wallet::class, $transfer->to->wallet);
 
         self::assertSame($buyer->wallet->getKey(), $transfer->from->getKey());
         self::assertSame($buyer->getKey(), $transfer->from->holder->getKey());
-        self::assertSame($product->getKey(), $transfer->to->getKey());
+        self::assertSame($product->wallet->getKey(), $transfer->to->getKey());
+        self::assertSame($product->getKey(), $transfer->to->holder->getKey());
     }
 
     public function testItemTransactions(): void
