74db1eb4e6850cdfb6ec31b0400bdab027b678ee
Goh Keat Liang <keatliang2005@gmail.com>
2022-07-28 20:12:17 +0800

    Transaction float accessor fix test

---
+++ tests/Units/Domain/TransactionAmountFloatAccessorTest.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bavix\Wallet\Test\Units\Domain;
+
+use Bavix\Wallet\Test\Infra\Factories\UserMultiFactory;
+use Bavix\Wallet\Test\Infra\Models\UserMulti;
+use Bavix\Wallet\Test\Infra\TestCase;
+
+/**
+ * @internal
+ */
+final class TransactionAmountFloatAccessorTest extends TestCase
+{
+    /**
+     * @see https://github.com/bavix/laravel-wallet/pull/533
+     */
+    public function testTransactionAmountFloatAccessor(): void
+    {
+        /** @var UserMulti $user */
+        $user = UserMultiFactory::new()->create();
+
+        // two decimal
+        $twoDecimalWallet = $user->createWallet([
+            'name' => '2 Floating point',
+            'slug' => '2-floating-point',
+            'decimal_places' => 2,
+        ]);
+
+        $amountTwoDecimal = 1.11;
+        $twoDecimalTransaction = $twoDecimalWallet->depositFloat($amountTwoDecimal);
+
+        self::assertNotNull($twoDecimalTransaction);
+        self::assertSame($amountTwoDecimal, (float) $twoDecimalTransaction->amountFloat, 'amount float is same decimal places');
+
+        // four decimal
+        $fourDecimalWallet = $user->createWallet([
+            'name' => '4 Floating point',
+            'slug' => '4-floating-point',
+            'decimal_places' => 4,
+        ]);
+
+        $amountFourDecimal = 1.1111;
+        $fourDecimalTransaction = $fourDecimalWallet->depositFloat($amountFourDecimal);
+
+        self::assertNotNull($fourDecimalTransaction);
+        self::assertSame($amountFourDecimal, (float) $fourDecimalTransaction->amountFloat, 'amount float is same decimal places');
+    }
+}
