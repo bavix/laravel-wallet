d6d8816780dc41eda6bedd307523be769eb89998
Maxim Babichev <maksim.babichev95@gmail.com>
2022-08-11 22:27:11 +0300

    add RaceCondTest

---
+++ tests/Units/Domain/RaceCondTest.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bavix\Wallet\Test\Units\Domain;
+
+use Bavix\Wallet\Test\Infra\Factories\BuyerFactory;
+use Bavix\Wallet\Test\Infra\Models\Buyer;
+use Bavix\Wallet\Test\Infra\TestCase;
+use Spatie\Fork\Fork;
+
+/**
+ * @internal
+ */
+final class RaceCondTest extends TestCase
+{
+    public function testSimple(): void
+    {
+        /** @var Buyer $buyer */
+        $buyer = BuyerFactory::new()->create();
+        $callback = static function () use ($buyer): void {
+            $buyer->deposit(1);
+        };
+
+        Fork::new()
+            ->concurrent(100)
+            ->run(...array_fill(1, 100, $callback));
+
+        self::assertSame(1_000, $buyer->balanceInt);
+    }
+}
