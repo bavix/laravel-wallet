0131c1367b27f3d29a89a2bffc5e4b21cbef48ee
Maxim Babichev <maksim.babichev95@gmail.com>
2023-01-04 12:20:46 +0300

    [10.x] remove CodeCoverageIgnore

--- src/WalletConfigure.php
+++ src/WalletConfigure.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet;
 
-#[\PHPUnit\Framework\Attributes\CodeCoverageIgnore]
 final class WalletConfigure
 {
     private static bool $runsMigrations = true;
