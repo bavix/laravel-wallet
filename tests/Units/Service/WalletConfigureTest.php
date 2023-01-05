be4d4ec3b7d9865966d0331cd729913ab7a3fc52
Maxim Babichev <maksim.babichev95@gmail.com>
2023-01-06 01:15:20 +0300

    [10.x] fix ServiceProvider

---
+++ tests/Units/Service/WalletConfigureTest.php
@@ -0,0 +1,22 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Bavix\Wallet\Test\Units\Service;
+
+use Bavix\Wallet\Test\Infra\TestCase;
+use Bavix\Wallet\WalletConfigure;
+
+/**
+ * @internal
+ */
+final class WalletConfigureTest extends TestCase
+{
+    public function testIgnoreMigrations(): void
+    {
+        self::assertTrue(WalletConfigure::isRunsMigrations());
+
+        WalletConfigure::ignoreMigrations();
+        self::assertFalse(WalletConfigure::isRunsMigrations());
+    }
+}
