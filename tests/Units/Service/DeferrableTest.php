d68c6efdd82a1f5e4864796b2db110c28082f3a0
Maxim Babichev <maksim.babichev95@gmail.com>
2023-01-06 01:25:16 +0300

    [10.x] fix

--- tests/Units/Service/DeferrableTest.php
+++ tests/Units/Service/DeferrableTest.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Bavix\Wallet\Test\Units\Service;
 
 use Bavix\Wallet\Test\Infra\TestCase;
-use Bavix\Wallet\WalletConfigure;
 use Bavix\Wallet\WalletServiceProvider;
 
 /**
