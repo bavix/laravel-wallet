02aa0762a45c9924bf752614dc81d3cdcd074439
Maxim Babichev <maksim.babichev95@gmail.com>
2023-01-06 10:08:33 +0300

    [10.x] update test migrations

--- tests/migrations/2014_10_12_000000_create_users_table.php
+++ tests/migrations/2014_10_12_000000_create_users_table.php
@@ -6,14 +6,10 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up()
+return new class() extends Migration {
+    public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('users', static function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
             $table->string('email');
@@ -21,11 +17,8 @@ class CreateUsersTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists('users');
     }
-}
+};
