cec5bfbbd0d5e717868bce65684d724512479ede
Maxim Babichev <maksim.babichev95@gmail.com>
2023-01-06 09:58:54 +0300

    [10.x] fix DeferrableTest

--- tests/Units/Service/DeferrableTest.php
+++ tests/Units/Service/DeferrableTest.php
@@ -6,6 +6,7 @@ namespace Bavix\Wallet\Test\Units\Service;
 
 use Bavix\Wallet\Test\Infra\TestCase;
 use Bavix\Wallet\WalletServiceProvider;
+use Illuminate\Contracts\Support\DeferrableProvider;
 
 /**
  * @internal
@@ -14,6 +15,11 @@ final class DeferrableTest extends TestCase
 {
     public function testIgnoreMigrations(): void
     {
-        self::assertNotEmpty(app()->resolveProvider(WalletServiceProvider::class)->provides());
+        $walletServiceProvider = app()
+            ->resolveProvider(WalletServiceProvider::class)
+        ;
+
+        self::assertInstanceOf(DeferrableProvider::class, $walletServiceProvider);
+        self::assertNotEmpty($walletServiceProvider->provides());
     }
 }
