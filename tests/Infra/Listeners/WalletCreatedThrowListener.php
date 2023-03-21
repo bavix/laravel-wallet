9187b29a847afab925861db983ca502bb3340ad3
Maxim Babichev <maksim.babichev95@gmail.com>
2023-03-21 21:21:25 +0300

    [10.x] phpstan part 14

--- tests/Infra/Listeners/WalletCreatedThrowListener.php
+++ tests/Infra/Listeners/WalletCreatedThrowListener.php
@@ -19,7 +19,7 @@ final class WalletCreatedThrowListener
         ;
 
         $message = hash('sha256', $holderType . $uuid . $createdAt);
-        $code = $walletCreatedEvent->getWalletId() + $walletCreatedEvent->getHolderId();
+        $code = $walletCreatedEvent->getWalletId() + (int) $walletCreatedEvent->getHolderId();
         assert($code > 1);
 
         throw new UnknownEventException($message, $code);
