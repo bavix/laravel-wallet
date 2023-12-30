83e3de51c9d9f629f478e7979dc5486a96389f41
Maxim Babichev <maksim.babichev95@gmail.com>
2023-12-30 20:34:48 +0300

    ecs fix

--- src/Services/BookkeeperService.php
+++ src/Services/BookkeeperService.php
@@ -8,7 +8,6 @@ use Bavix\Wallet\Internal\Exceptions\RecordNotFoundException;
 use Bavix\Wallet\Internal\Service\LockServiceInterface;
 use Bavix\Wallet\Internal\Service\StorageServiceInterface;
 use Bavix\Wallet\Models\Wallet;
-use PHPUnit\Framework\Attributes\CodeCoverageIgnore;
 
 /**
  * @internal
