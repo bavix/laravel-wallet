0137591d2d4dac2577af2f70c332b2da8ed0ed9e
Maxim Babichev <maksim.babichev95@gmail.com>
2023-12-31 08:00:03 +0300

    add units

--- tests/Units/Domain/SoftDeletesTest.php
+++ tests/Units/Domain/SoftDeletesTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Test\Units\Domain;
 
+use Bavix\Wallet\Exceptions\UnconfirmedInvalid;
 use Bavix\Wallet\Test\Infra\Factories\BuyerFactory;
 use Bavix\Wallet\Test\Infra\Models\Buyer;
 use Bavix\Wallet\Test\Infra\TestCase;
@@ -11,7 +12,7 @@ use Bavix\Wallet\Test\Infra\TestCase;
 /**
  * @internal
  */
-final class SoftDeleteTest extends TestCase
+final class SoftDeletesTest extends TestCase
 {
     public function testDefaultWalletSoftDelete(): void
     {
@@ -81,6 +82,28 @@ final class SoftDeleteTest extends TestCase
 
         self::assertSame(0, $buyer->balanceInt);
         self::assertFalse($transaction->confirmed);
+        self::assertNotNull($transaction->deleted_at);
+    }
+
+    public function testTransactionDeleteIfNotConfirmed(): void
+    {
+        $this->expectException(UnconfirmedInvalid::class);
+
+        /** @var Buyer $buyer */
+        $buyer = BuyerFactory::new()->create();
+        self::assertFalse($buyer->relationLoaded('wallet'));
+        self::assertFalse($buyer->wallet->exists);
+
+        $transaction = $buyer->deposit(1);
+
+        self::assertTrue($buyer->wallet->exists);
+        self::assertSame(1, $buyer->balanceInt);
+
+        self::assertTrue($buyer->wallet->resetConfirm($transaction));
+
+        self::assertSame(0, $buyer->balanceInt);
+
+        self::assertTrue($transaction->delete());
     }
 
     public function testTransferDelete(): void
@@ -104,9 +127,46 @@ final class SoftDeleteTest extends TestCase
         self::assertSame(-100, $user1->balanceInt);
         self::assertSame(100, $user2->balanceInt);
 
+        self::assertTrue($transfer->deposit->confirmed);
+        self::assertTrue($transfer->withdraw->confirmed);
+
         self::assertTrue($transfer->delete());
 
         self::assertSame(0, $user1->balanceInt);
         self::assertSame(0, $user2->balanceInt);
+
+        self::assertFalse($transfer->deposit->confirmed);
+        self::assertFalse($transfer->withdraw->confirmed);
+    }
+
+    public function testTransferDeleteIfNotConfirmed(): void
+    {
+        $this->expectException(UnconfirmedInvalid::class);
+
+        /** @var Buyer $user1 */
+        /** @var Buyer $user2 */
+        [$user1, $user2] = BuyerFactory::times(2)->create();
+
+        self::assertFalse($user1->relationLoaded('wallet'));
+        self::assertFalse($user1->wallet->exists);
+
+        self::assertFalse($user2->relationLoaded('wallet'));
+        self::assertFalse($user2->wallet->exists);
+
+        $transfer = $user1->forceTransfer($user2, 100);
+
+        self::assertNotNull($transfer);
+        self::assertSame(100, $transfer->deposit->amountInt);
+        self::assertSame(-100, $transfer->withdraw->amountInt);
+
+        self::assertSame(-100, $user1->balanceInt);
+        self::assertSame(100, $user2->balanceInt);
+
+        self::assertTrue($transfer->deposit->confirmed);
+        self::assertTrue($transfer->withdraw->confirmed);
+
+        self::assertTrue($user1->wallet->resetConfirm($transfer->withdraw));
+
+        self::assertTrue($transfer->delete());
     }
 }
