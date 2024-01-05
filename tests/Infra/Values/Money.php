7042fcb381b027cd5d11818a9f6d7037266f7222
Maxim Babichev <maksim.babichev95@gmail.com>
2024-01-05 19:44:48 +0300

    drop depends

--- tests/Infra/Values/Money.php
+++ tests/Infra/Values/Money.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Test\Infra\Values;
 
-final class Money
+final readonly class Money
 {
     public function __construct(
-        public readonly string $amount,
-        public readonly string $currency,
+        public string $amount,
+        public string $currency,
     ) {
     }
 }
