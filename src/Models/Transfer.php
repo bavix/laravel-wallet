c4cad87cbd6f8c7a84bf21338baad27fd980a65e
George Klincarski <g.klincarski@gmail.com>
2024-01-23 20:20:45 +0100

    Revert MorphTo into BelongsTo
    Signed-off-by: George Klincarski <g.klincarski@gmail.com>

--- src/Models/Transfer.php
+++ src/Models/Transfer.php
@@ -6,7 +6,6 @@ namespace Bavix\Wallet\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\MorphTo;
 use function config;
 
 /**
@@ -80,17 +79,17 @@ class Transfer extends Model
     /**
      * @return BelongsTo<Wallet, self>
      */
-    public function from(): MorphTo
+    public function from(): BelongsTo
     {
-        return $this->morphTo();
+        return $this->belongsTo(config('wallet.wallet.model', Wallet::class), 'from_id');
     }
 
     /**
      * @return BelongsTo<Wallet, self>
      */
-    public function to(): MorphTo
+    public function to(): BelongsTo
     {
-        return $this->morphTo();
+        return $this->belongsTo(config('wallet.wallet.model', Wallet::class), 'to_id');
     }
 
     /**
