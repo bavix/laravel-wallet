e83b5f1c25ea21af427b7bb1bad680a238cd02ad
Maxim Babichev <maksim.babichev95@gmail.com>
2024-01-25 08:14:24 +0300

    Added *FloatQuery for High performance api handles

--- tests/Units/Api/TransferHandlerTest.php
+++ tests/Units/Api/TransferHandlerTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Test\Units\Api;
 
+use Bavix\Wallet\External\Api\TransferFloatQuery;
 use Bavix\Wallet\External\Api\TransferQuery;
 use Bavix\Wallet\External\Api\TransferQueryHandlerInterface;
 use Bavix\Wallet\External\Dto\Extra;
@@ -57,14 +58,22 @@ final class TransferHandlerTest extends TestCase
                     '7f0175fe-99cc-4058-92c6-157f0da18243',
                     ['type' => 'third'],
                 )),
+            new TransferFloatQuery($to, $from, .50,
+                new Extra(
+                    null,
+                    null,
+                    '1a7326a6-dfdf-4ec8-afc4-cb21cf1f43c6',
+                    ['type' => 'fourth'],
+                )),
         ]);
 
-        self::assertSame(-150, $from->balanceInt);
-        self::assertSame(150, $to->balanceInt);
-        self::assertCount(3, $transfers);
+        self::assertSame(-100, $from->balanceInt);
+        self::assertSame(100, $to->balanceInt);
+        self::assertCount(4, $transfers);
 
         self::assertSame(['type' => 'first'], $transfers['598c184c-e6d6-4fc2-9640-c1c7acb38093']->extra);
         self::assertSame(['type' => 'second'], $transfers['f303d60d-c2de-45d0-b9ed-e1487429709a']->extra);
         self::assertSame(['type' => 'third'], $transfers['7f0175fe-99cc-4058-92c6-157f0da18243']->extra);
+        self::assertSame(['type' => 'fourth'], $transfers['1a7326a6-dfdf-4ec8-afc4-cb21cf1f43c6']->extra);
     }
 }
