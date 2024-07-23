094c96243c91628d270d6629db664ac8142dfc39
Maxim Babichev <maksim.babichev95@gmail.com>
2024-07-23 21:24:26 +0300

    fix TransferDtoAssember

--- src/Internal/Assembler/TransferDtoAssemblerInterface.php
+++ src/Internal/Assembler/TransferDtoAssemblerInterface.php
@@ -9,8 +9,19 @@ use Illuminate\Database\Eloquent\Model;
 
 interface TransferDtoAssemblerInterface
 {
-    /**
+/**
      * Create transfer dto.
+     *
+     * @param int $depositId ID of deposit transaction
+     * @param int $withdrawId ID of withdraw transaction
+     * @param string $status Status of transfer
+     * @param Model $fromModel From wallet model
+     * @param Model $toModel To wallet model
+     * @param int $discount Discount of transfer
+     * @param string $fee Fee of transfer
+     * @param string|null $uuid UUID of transfer
+     * @param array<mixed>|null $extra Extra data of transfer
+     * @return TransferDtoInterface
      */
     public function create(
         int $depositId,
