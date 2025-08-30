ee69deb24c2b928f6116137dae80de845f4c883b
Maxim Babichev <maksim.babichev95@gmail.com>
2025-08-30 20:06:00 +0300

    trim event phpdoc

--- src/Internal/Events/TransactionCreatedEventInterface.php
+++ src/Internal/Events/TransactionCreatedEventInterface.php
@@ -9,31 +9,11 @@ use DateTimeImmutable;
 
 interface TransactionCreatedEventInterface extends EventInterface
 {
-    /**
-     * Returns the ID of the transaction.
-     *
-     * @return int The transaction ID.
-     */
     public function getId(): int;
 
-    /**
-     * Returns the type of the transaction.
-     *
-     * @return TransactionType The transaction type.
-     */
     public function getType(): TransactionType;
 
-    /**
-     * Returns the ID of the wallet associated with the transaction.
-     *
-     * @return int The wallet ID.
-     */
     public function getWalletId(): int;
 
-    /**
-     * Returns the creation date and time of the transaction.
-     *
-     * @return DateTimeImmutable The creation date and time.
-     */
     public function getCreatedAt(): DateTimeImmutable;
 }
