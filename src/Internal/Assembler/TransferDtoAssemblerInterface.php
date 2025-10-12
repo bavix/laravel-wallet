5d54d04e873ee7626daefdcbc6f23d7ed9f6c939
Maxim Babichev <maksim.babichev95@gmail.com>
2025-10-12 19:43:00 +0300

    simplify transfer dto docs

--- src/Internal/Assembler/TransferDtoAssemblerInterface.php
+++ src/Internal/Assembler/TransferDtoAssemblerInterface.php
@@ -11,17 +11,7 @@ use Illuminate\Database\Eloquent\Model;
 interface TransferDtoAssemblerInterface
 {
     /**
-     * Create transfer dto.
-     *
-     * @param int $depositId ID of deposit transaction
-     * @param int $withdrawId ID of withdraw transaction
-     * @param TransferStatus $status Status of transfer
-     * @param Model $fromModel From wallet model
-     * @param Model $toModel To wallet model
-     * @param int $discount Discount of transfer
-     * @param string $fee Fee of transfer
-     * @param string|null $uuid UUID of transfer
-     * @param array<mixed>|null $extra Extra data of transfer
+     * @param array<mixed>|null $extra
      */
     public function create(
         int $depositId,
