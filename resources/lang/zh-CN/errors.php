ecbc9a6eb2544e07c15cec6990e8ec4e1607c763
Maxim Babichev <maksim.babichev95@gmail.com>
2026-02-21 19:56:00 +0300

    add upgrade notes for v12

--- resources/lang/zh-CN/errors.php
+++ resources/lang/zh-CN/errors.php
@@ -8,6 +8,6 @@ return [
     'wallet_empty' => '钱包为空',
     'insufficient_funds' => '余额不足',
     'confirmed_invalid' => '交易已经确认',
-    'unconfirmed_invalid' => '確認已被重置',
+    'unconfirmed_invalid' => '确认已被重置',
     'owner_invalid' => '你不是钱包的主人',
 ];
