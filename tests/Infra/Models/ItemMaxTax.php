b35e0e0473e0038ea47a575bbed62bac8924cfae
Maxim Babichev <maksim.babichev95@gmail.com>
2026-03-13 01:49:40 +0300

    format tax item checks

--- tests/Infra/Models/ItemMaxTax.php
+++ tests/Infra/Models/ItemMaxTax.php
@@ -4,11 +4,11 @@ declare(strict_types=1);
 
 namespace Bavix\Wallet\Test\Infra\Models;
 
+use Bavix\Wallet\External\Api\PurchaseQuery;
+use Bavix\Wallet\External\Api\PurchaseQueryHandlerInterface;
 use Bavix\Wallet\Interfaces\Customer;
 use Bavix\Wallet\Interfaces\MaximalTaxable;
 use Bavix\Wallet\Interfaces\ProductLimitedInterface;
-use Bavix\Wallet\External\Api\PurchaseQuery;
-use Bavix\Wallet\External\Api\PurchaseQueryHandlerInterface;
 use Bavix\Wallet\Models\Wallet;
 use Bavix\Wallet\Services\CastService;
 use Bavix\Wallet\Traits\HasWallet;
@@ -44,7 +44,9 @@ final class ItemMaxTax extends Model implements ProductLimitedInterface, Maximal
             return $result;
         }
 
-        return $result && ! app(PurchaseQueryHandlerInterface::class)->one(PurchaseQuery::create($customer, $this)) instanceof \Bavix\Wallet\Models\Transfer;
+        return $result && ! app(PurchaseQueryHandlerInterface::class)->one(
+            PurchaseQuery::create($customer, $this)
+        ) instanceof \Bavix\Wallet\Models\Transfer;
     }
 
     public function getAmountProduct(Customer $customer): int
