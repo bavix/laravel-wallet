49f63aa8bba857746c2c532a9b6227bbe7db09cb
Maxim Babichev <maksim.babichev95@gmail.com>
2026-03-13 23:32:48 +0300

    refactor purchase query and relation methods

--- src/External/Api/PurchaseQueryHandler.php
+++ src/External/Api/PurchaseQueryHandler.php
@@ -23,6 +23,42 @@ final readonly class PurchaseQueryHandler implements PurchaseQueryHandlerInterfa
 
     public function apply(array $objects): array
     {
+        ['queryMap' => $queryMap, 'fromIds' => $fromIds, 'toIds' => $toIds] = $this->buildQueryMap($objects);
+
+        $matchedTransferIds = $this->matchByPurchases($fromIds, $toIds);
+        $needFallback = $this->collectFallbackKeys($queryMap, $matchedTransferIds);
+
+        if ($needFallback !== []) {
+            $matchedTransferIds = $this->matchByTransfersFallback($fromIds, $toIds, $needFallback, $matchedTransferIds);
+        }
+
+        $matchedTransfers = $this->loadMatchedTransfers($matchedTransferIds);
+
+        return $this->buildResult($queryMap, $matchedTransferIds, $matchedTransfers);
+    }
+
+    public function one(PurchaseQueryInterface $query): ?Transfer
+    {
+        $result = $this->apply([$query]);
+
+        return $result[0] ?? null;
+    }
+
+    private function key(int $fromId, int $toId, bool $includeGifts): string
+    {
+        return $fromId.':'.$toId.':'.($includeGifts ? '1' : '0');
+    }
+
+    /**
+     * @param array<int, PurchaseQueryInterface> $objects
+     * @return array{
+     *     queryMap: array<int, array{strict: string, gifts: string, includeGifts: bool}>,
+     *     fromIds: array<int, true>,
+     *     toIds: array<int, true>
+     * }
+     */
+    private function buildQueryMap(array $objects): array
+    {
         $queryMap = [];
         $fromIds = [];
         $toIds = [];
@@ -43,6 +79,20 @@ final readonly class PurchaseQueryHandler implements PurchaseQueryHandlerInterfa
             ];
         }
 
+        return [
+            'queryMap' => $queryMap,
+            'fromIds' => $fromIds,
+            'toIds' => $toIds,
+        ];
+    }
+
+    /**
+     * @param array<int, true> $fromIds
+     * @param array<int, true> $toIds
+     * @return array<string, int>
+     */
+    private function matchByPurchases(array $fromIds, array $toIds): array
+    {
         $matchedTransferIds = [];
         $purchases = $this->purchase->newQuery()
             ->select(['id', 'transfer_id', 'from_id', 'to_id', 'status'])
@@ -64,6 +114,16 @@ final readonly class PurchaseQueryHandler implements PurchaseQueryHandlerInterfa
             }
         }
 
+        return $matchedTransferIds;
+    }
+
+    /**
+     * @param array<int, array{strict: string, gifts: string, includeGifts: bool}> $queryMap
+     * @param array<string, int> $matchedTransferIds
+     * @return array<string, true>
+     */
+    private function collectFallbackKeys(array $queryMap, array $matchedTransferIds): array
+    {
         $needFallback = [];
         foreach ($queryMap as $item) {
             $strict = $item['strict'];
@@ -77,64 +137,88 @@ final readonly class PurchaseQueryHandler implements PurchaseQueryHandlerInterfa
             }
         }
 
-        if ($needFallback !== []) {
-            $transfers = $this->transfer->newQuery()
-                ->select([$this->transfer->getKeyName(), 'from_id', 'to_id', 'status'])
-                ->whereIn('from_id', array_keys($fromIds))
-                ->whereIn('to_id', array_keys($toIds))
-                ->whereIn('status', [TransferStatus::Paid->value, TransferStatus::Gift->value])
-                ->orderByDesc($this->transfer->getKeyName())
-                ->get();
+        return $needFallback;
+    }
 
-            foreach ($transfers as $transfer) {
-                $strictKey = $this->key($transfer->from_id, $transfer->to_id, false);
-                if (
-                    $transfer->status === TransferStatus::Paid
-                    && array_key_exists($strictKey, $needFallback)
-                    && ! array_key_exists($strictKey, $matchedTransferIds)
-                ) {
-                    $matchedTransferIds[$strictKey] = $transfer->getKey();
-                }
+    /**
+     * @param array<int, true> $fromIds
+     * @param array<int, true> $toIds
+     * @param array<string, true> $needFallback
+     * @param array<string, int> $matchedTransferIds
+     * @return array<string, int>
+     */
+    private function matchByTransfersFallback(
+        array $fromIds,
+        array $toIds,
+        array $needFallback,
+        array $matchedTransferIds
+    ): array {
+        $transfers = $this->transfer->newQuery()
+            ->select([$this->transfer->getKeyName(), 'from_id', 'to_id', 'status'])
+            ->whereIn('from_id', array_keys($fromIds))
+            ->whereIn('to_id', array_keys($toIds))
+            ->whereIn('status', [TransferStatus::Paid->value, TransferStatus::Gift->value])
+            ->orderByDesc($this->transfer->getKeyName())
+            ->get();
 
-                $giftsKey = $this->key($transfer->from_id, $transfer->to_id, true);
-                if (array_key_exists($giftsKey, $needFallback) && ! array_key_exists($giftsKey, $matchedTransferIds)) {
-                    $matchedTransferIds[$giftsKey] = $transfer->getKey();
-                }
+        foreach ($transfers as $transfer) {
+            $strictKey = $this->key($transfer->from_id, $transfer->to_id, false);
+            if (
+                $transfer->status === TransferStatus::Paid
+                && array_key_exists($strictKey, $needFallback)
+                && ! array_key_exists($strictKey, $matchedTransferIds)
+            ) {
+                $matchedTransferIds[$strictKey] = $transfer->getKey();
+            }
+
+            $giftsKey = $this->key($transfer->from_id, $transfer->to_id, true);
+            if (array_key_exists($giftsKey, $needFallback) && ! array_key_exists($giftsKey, $matchedTransferIds)) {
+                $matchedTransferIds[$giftsKey] = $transfer->getKey();
             }
         }
 
+        return $matchedTransferIds;
+    }
+
+    /**
+     * @param array<string, int> $matchedTransferIds
+     * @return array<int, Transfer>
+     */
+    private function loadMatchedTransfers(array $matchedTransferIds): array
+    {
         $matched = [];
         $transferIds = array_values(array_unique($matchedTransferIds));
-        if ($transferIds !== []) {
-            $transfers = $this->transfer->newQuery()
-                ->with(['deposit', 'withdraw.wallet'])
-                ->whereIn($this->transfer->getKeyName(), $transferIds)
-                ->get();
-
-            foreach ($transfers as $transfer) {
-                $matched[$transfer->getKey()] = $transfer;
-            }
+        if ($transferIds === []) {
+            return $matched;
         }
 
+        $transfers = $this->transfer->newQuery()
+            ->with(['deposit', 'withdraw.wallet'])
+            ->whereIn($this->transfer->getKeyName(), $transferIds)
+            ->get();
+
+        foreach ($transfers as $transfer) {
+            $matched[$transfer->getKey()] = $transfer;
+        }
+
+        return $matched;
+    }
+
+    /**
+     * @param array<int, array{strict: string, gifts: string, includeGifts: bool}> $queryMap
+     * @param array<string, int> $matchedTransferIds
+     * @param array<int, Transfer> $matchedTransfers
+     * @return array<int, Transfer|null>
+     */
+    private function buildResult(array $queryMap, array $matchedTransferIds, array $matchedTransfers): array
+    {
         $result = [];
         foreach ($queryMap as $index => $item) {
             $key = $item['includeGifts'] ? $item['gifts'] : $item['strict'];
             $transferId = $matchedTransferIds[$key] ?? null;
-            $result[$index] = is_int($transferId) ? ($matched[$transferId] ?? null) : null;
+            $result[$index] = is_int($transferId) ? ($matchedTransfers[$transferId] ?? null) : null;
         }
 
         return $result;
-    }
-
-    public function one(PurchaseQueryInterface $query): ?Transfer
-    {
-        $result = $this->apply([$query]);
-
-        return $result[0] ?? null;
-    }
-
-    private function key(int $fromId, int $toId, bool $includeGifts): string
-    {
-        return $fromId.':'.$toId.':'.($includeGifts ? '1' : '0');
     }
 }
